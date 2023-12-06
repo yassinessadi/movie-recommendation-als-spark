fe946722d9dfe556985ab7c033d122473bacbf92
yassinessadi <yasine.esadi@gmail.com>
2023-12-06 18:32:12 +0100

    create a kafka producer with confluent kafka lib/

--- app/producer.py
+++ app/producer.py
@@ -3,7 +3,7 @@ import requests
 import json
 import logging
 
-topic = "ratingmoviesapplication"
+topic = "ratingmoviestopic"
 kafka_config = {
     "bootstrap.servers": "localhost:9092", 
 }
@@ -12,7 +12,7 @@ kafka_config = {
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# producer = Producer(kafka_config)
+producer = Producer(kafka_config)
 counter = 0
 while True:
     base_url = f"http://127.0.0.1:5000/api/movie"
@@ -22,8 +22,8 @@ while True:
     response = requests.get(base_url,params=params)
     if response.status_code == 200:
         data = json.dumps(response.json())
-        # producer.produce(topic, key="key", value=data)
-        # producer.flush()
+        producer.produce(topic, key="key", value=data)
+        producer.flush()
         logger.info("Produced message: %s", data)
         counter += 1
     else:
