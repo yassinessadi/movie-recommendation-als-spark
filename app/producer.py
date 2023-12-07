8ec532c60765a40ce34a2ccae91501060eb175c2
yassinessadi <yasine.esadi@gmail.com>
2023-12-07 16:13:02 +0100

    add commit and clean code

--- app/producer.py
+++ app/producer.py
@@ -3,21 +3,39 @@ import requests
 import json
 import logging
 
+
+#----+----+----+----+----+#
+#       topic name        #
+#----+----+----+----+----+#
 topic = "ratingmoviestopic"
+
+#----+----+----+----+----+#
+#       kafka broker      #
+#----+----+----+----+----+#
 kafka_config = {
     "bootstrap.servers": "localhost:9092", 
 }
 
-# Configure logging
+
+#----+----+----+----+----+#
+#     Configure logging   #
+#----+----+----+----+----+#
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+
+#----+----+----+----+----+#
+#    init kafk producer   #
+#----+----+----+----+----+#
 producer = Producer(kafka_config)
+
+
 counter = 0
 while True:
     base_url = f"http://127.0.0.1:5000/api/movie"
     params = {
-        "ID" : counter
+        "id" : counter
     }
     response = requests.get(base_url,params=params)
     if response.status_code == 200:
@@ -27,5 +45,4 @@ while True:
         logger.info("Produced message: %s", data)
         counter += 1
     else:
-        print(response.status_code)
         logger.error("Failed to fetch movie data. HTTP status code: %d", response.status_code)
